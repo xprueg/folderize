79b4f77aca287d007d9de562b2ea00ee7b7ac53a
Patrick Rügheimer <ptrckr@users.noreply.github.com>
2018-10-10 14:07:22 +0200

    Put functions in a class, added progressbar

--- folderize.py
+++ folderize.py
@@ -15,58 +15,85 @@ def main():
         print("Error: No path argument specified!", file=sys.stderr)
     else:
         root = sys.argv[1]
-        if os.path.isdir(root):
-            folderize(root)
-        else:
-            print("Error: The argument ``{argv}'' is not a valid path".format(argv=root), file=sys.stderr)
+        Folderize(root)
 
-def iterate_files(path, callback):
-    with os.scandir(path) as it:
-        for file in it:
-            if not file.name.startswith(".") and file.is_file():
-                if callback(file.path):
-                    return True
+class Folderize:
+    def __init__(self, root):
+        self.root = root
+        if not os.path.isdir(root):
+            raise ValueError("Error: The argument `{argv}' is not a valid directory".format(argv=root))
 
-    return False
+        self.filecount = self.get_file_count(self.root)
+        self.current_index = 0
 
-def folderize(folder):
-    iterate_files(folder, lambda file: maybe_copy(file, get_dst_path(folder, file)))
+        self.iterate_files(self.root, self.maybe_copy, self.update_progressbar)
 
-def maybe_copy(src_file, dst_folder):
-    basename = os.path.basename(src_file)
-    splitname = os.path.splitext(basename)
-    dst_file = os.path.join(dst_folder, basename)
+    def iterate_files(self, path, func, callback=None):
+        with os.scandir(path) as it:
+            for file in it:
+                if not file.name.startswith(".") and file.is_file():
+                    if callback is not None:
+                        callback(file.path)
 
-    tmp_file_i = 0
+                    if func(file.path):
+                        return True
 
-    while True:
-        # Filename exists
-        if os.path.isfile(dst_file):
-            
-            # Same name, same contents
-            if filecmp.cmp(src_file, dst_file):
+    def update_progressbar(self, file):
+        self.current_index += 1
+        print("\rCopied {i} of {filecount} files @ {progress} %".format(
+            i=self.current_index,
+            filecount=self.filecount,
+            progress=int(100 / self.filecount * (self.current_index))
+        ), end="" if (self.current_index < self.filecount) else "\n")
+
+    def maybe_copy(self, src):
+        src_base = os.path.basename(src)
+        src_splitname = os.path.splitext(src_base)
+        dst_folder = self.get_dst_folder(src)
+        dst = os.path.join(dst_folder, src_base)
+
+        tmp_file_i = 0
+
+        while True:
+            # Filename exists
+            if os.path.isfile(dst):
+
+                # Same name, same contents
+                if filecmp.cmp(src, dst):
+                    break
+
+                # Same name, different contents
+                else:
+                    tmp_filename = "{name}_{i}{ext}".format(
+                        name=src_splitname[0],
+                        i=tmp_file_i,
+                        ext=src_splitname[1]
+                    )
+                    dst = os.path.join(dst_folder, tmp_filename)
+                    tmp_file_i += 1
+
+            # File with different name has same contents (within target folder)
+            elif os.path.isdir(dst_folder) and self.iterate_files(dst_folder, lambda file: filecmp.cmp(src, file)):
                 break
-                
-            # Same name, different contents
-            else:
-                dst_file = os.path.join(dst_folder, "{name}_{i}{ext}".format(name=splitname[0],
-                                                                             i=tmp_file_i,
-                                                                             ext=splitname[1]))
-                tmp_file_i += 1
-                
-        # File with different name has same contents (within target folder)
-        elif os.path.isdir(dst_folder) and iterate_files(dst_folder, lambda file: filecmp.cmp(src_file, file)):
-            break
-            
-        # Neither filename nor contents match, create it
-        else:
-            pathlib.Path(dst_folder).mkdir(parents=True, exist_ok=True)
-            shutil.copy2(src_file, dst_file)
-            break
 
-def get_dst_path(root, file):
-    cdate = datetime.datetime.fromtimestamp(os.path.getmtime(file))
-    return os.path.join(root, str(cdate.year), calendar.month_name[cdate.month], str(cdate.day))
+            # Filename nor contents match, create it
+            else:
+                pathlib.Path(dst_folder).mkdir(parents=True, exist_ok=True)
+                shutil.copy2(src, dst)
+                break
+
+    def get_file_count(self, path):
+        with os.scandir(path) as it:
+            return len([x for x in it if not x.name.startswith(".") and x.is_file()])
+
+    def get_dst_folder(self, file):
+        cdate = datetime.datetime.fromtimestamp(os.path.getmtime(file))
+        return os.path.join(
+            self.root,
+            str(cdate.year),
+            calendar.month_name[cdate.month],
+            str(cdate.day)
+        )
 
 if __name__ == "__main__":
     main()
