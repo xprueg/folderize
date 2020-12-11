ef6ec3c27bf20f8d381a1dc91385ce662511e981
xprueg <github@pr.link>
2020-12-11 22:28:55 +0100

    Support multiline strings

--- utils/console.mjs
+++ utils/console.mjs
@@ -1,4 +1,6 @@
 import { moveCursor, clearLine } from "readline";
+import { EOL } from "os";
+
 import fmt_ansi_esc_codes from "./fmt_ansi_esc.mjs";
 
 /**
@@ -22,32 +24,40 @@ get_timestamp.formatter = new Intl.DateTimeFormat(
 /**
  * Prints to stdout.
  * A timestamp will be prepended if [msg] is defined.
- * @param {string} [msg=""] - The message to be printed.
+ * @param {string} [msg=String()] - Message to be printed.
  * @returns {void}
  */
 export function println(msg = String()) {
-  if (msg !== String())
-    msg = `[f]${get_timestamp()}[/f]\x20` + msg;
+  if (msg === String())
+    return void console.error(msg);
 
-  console.log(fmt_ansi_esc_codes(msg));
+  console.error(fmt_ansi_esc_codes(`${
+    msg.split(EOL)
+       .map(line => `[f]${get_timestamp()}[/f]\x20${line}`)
+       .join(EOL)
+  }`));
 }
 
 /**
  * Prints to stderr.
  * A timestamp will be prepended if [msg] is defined.
- * @param {string} [msg=""] - The message to be printed.
+ * @param {string} [msg=String()] - Message to be printed.
  * @returns {void}
  */
 export function eprintln(msg = String()) {
-  if (msg !== String())
-    msg = `[f]${get_timestamp()}[/f]\x20` + msg;
+  if (msg === String())
+    return void console.error(msg);
 
-  console.error(fmt_ansi_esc_codes(`[red]${msg}[/red]`));
+  console.error(fmt_ansi_esc_codes(`[red]${
+    msg.split(EOL)
+       .map(line => `[f]${get_timestamp()}[/f]\x20${line}`)
+       .join(EOL)
+  }[/red]`));
 }
 
 /**
  * Prints to stdout over the cleared previous line.
- * @param {string} [msg=""] - The message to be printed.
+ * @param {string} [msg=String()] - Message to be printed.
  * @returns {void}
  */
 export function printover(msg = String()) {
