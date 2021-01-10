6546c477030b208128af2412f66b490bcb9a0f10
xprueg <github@pr.link>
2021-01-10 21:53:57 +0100

    Removed faulty function mkdir
    The function tried finding existing folders but failed to do so properly.
    First it had issues with umlauts because the path name was not beeing normalized.
    Then to allow for foldername changes it tried to match the foldername with startsWith.
    This worked but alas reported false positives, like "10" for "1".
    
    Thus now a hidden file gets created in every folder for matching them, which allows for complete control over the foldername.

--- utils/datedir.mjs
+++ utils/datedir.mjs
@@ -1,13 +1,14 @@
-import path from "path";
-import fs from "fs";
-
 import { panic } from "./panic.mjs";
-import { Read } from "./fs.mjs";
 
 export default class DateDir {
     #struct;
     #dtfmt;
 
+    /// Creates a new `DateDir` instance.
+    ///
+    /// [>] struct :: string
+    /// [>] locale :: string
+    /// [<] DateDir
     constructor(struct, locale) {
         this.#struct = struct;
         this.#dtfmt = {
@@ -17,7 +18,7 @@ export default class DateDir {
             b: new Intl.DateTimeFormat(locale, { month: "short" }),
             B: new Intl.DateTimeFormat(locale, { month: "long" }),
             Y: new Intl.DateTimeFormat(locale, { year: "numeric" }),
-            y: new Intl.DateTimeFormat(locale, { year: "2-digit" })
+            y: new Intl.DateTimeFormat(locale, { year: "2-digit" }),
         };
     }
 
@@ -39,37 +40,5 @@ export default class DateDir {
             /%(\w)/g,
             (_, opt) => this.#dtfmt[opt].format(date)
         );
-    }
-
-    /// Recursively creates the formatted "self.#struct" path for the given date.
-    ///
-    /// [>] date: Date
-    /// [!] Panic
-    /// [<] string
-    mkdir(root, date) {
-        let dir = root;
-        
-        if (this.#struct === String())
-            return dir;
-
-        try {
-            this.fmt(date)
-                .split("/")
-                .forEach(segment => {
-                    const folder = Read.dir(dir).collect(Read.DIR);
-
-                    // Search and return existing folder.
-                    for (let f of folder)
-                        if (path.basename(f).normalize().startsWith(segment))
-                            return void ((dir = f));
-
-                    // Create non existing folder.
-                    fs.mkdirSync((dir = path.join(dir, segment)));
-                });
-
-            return dir;
-        } catch(err) {
-            panic(err)`Could not create formatted directory`;
-        }
     }
 }
