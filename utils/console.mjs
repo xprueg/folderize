5cf797ffef26c0af7f0decd4ff8dc1ba9a7aa8d0
xprueg <github@pr.link>
2021-01-11 22:15:22 +0100

    Revised documentation, added helper function

--- utils/console.mjs
+++ utils/console.mjs
@@ -3,15 +3,16 @@ import { EOL } from "os";
 
 import fmt_ansi_esc_codes from "./fmt_ansi_esc.mjs";
 
-/**
- * Returns a formatted timestamp.
- * @returns {string}
- */
+/// Returns a formatted timestamp.
+///
+/// [<] string
 function get_timestamp() {
     return get_timestamp.formatter.format(Date.now());
 }
 
-/** @member {DateTimeFormat} */
+/// The static formatter of the `get_timestamp` function.
+///
+/// <T> DateTimeFormat
 get_timestamp.formatter = new Intl.DateTimeFormat(
     "en-US", {
       hour: "2-digit",
@@ -21,47 +22,54 @@ get_timestamp.formatter = new Intl.DateTimeFormat(
     }
 );
 
-/**
- * Prints to stdout.
- * A timestamp will be prepended if [msg] is defined.
- * @param {string} [msg=String()] - Message to be printed.
- * @returns {void}
- */
+/// Prints either to stdout or stderr depending on the argument "fn".
+/// A timestamp will be prepended if "msg" is defined,
+/// otherwise an empty line will be printed.
+///
+/// [>] fn[val = log|error] :: string
+/// [>] msg :: string
+/// [<] void
+function __print(fn, msg) {
+  if (msg === String())
+    return void console[fn](msg);
+
+  msg = msg
+    .split(EOL)
+    .map(line => `[f]${get_timestamp()}[/f]\x20${line}`)
+    .join(EOL);
+
+  if (fn === "error")
+    msg = `[red]${msg}[/red]`;
+
+  console[fn](fmt_ansi_esc_codes(msg));
+}
+
+/// Prints to stdout.
+/// A timestamp will be prepended if "msg" is defined,
+/// otherwise an empty line will be printed.
+///
+/// [>] msg[? = ""] :: string
+/// [<] void
 export function println(msg = String()) {
-  if (msg === String())
-    return void console.error(msg);
-
-  console.error(fmt_ansi_esc_codes(`${
-    msg.split(EOL)
-       .map(line => `[f]${get_timestamp()}[/f]\x20${line}`)
-       .join(EOL)
-  }`));
+  __print("log", msg);
 }
 
-/**
- * Prints to stderr.
- * A timestamp will be prepended if [msg] is defined.
- * @param {string} [msg=String()] - Message to be printed.
- * @returns {void}
- */
+/// Prints to stderr.
+/// A timestamp will be prepended if "msg" is defined,
+/// otherwise an empty line will be printed.
+///
+/// [>] msg[? = ""] :: string
+/// [<] void
 export function eprintln(msg = String()) {
-  if (msg === String())
-    return void console.error(msg);
-
-  console.error(fmt_ansi_esc_codes(`[red]${
-    msg.split(EOL)
-       .map(line => `[f]${get_timestamp()}[/f]\x20${line}`)
-       .join(EOL)
-  }[/red]`));
+  __print("error", msg);
 }
 
-/**
- * Prints to stdout over the cleared previous line.
- * @param {string} [msg=String()] - Message to be printed.
- * @returns {void}
- */
+/// Prints over the cleared prevoius line to stdout.
+///
+/// [>] msg[? = ""] :: string
+/// [<] void
 export function printover(msg = String()) {
   moveCursor(process.stdin, 0, -1);
   clearLine(process.stdin, 0);
-  println(msg);
+  __print("log", msg);
 }
